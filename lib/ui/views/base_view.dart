ff5e9aaf196954911da92e80f8d0833590bd1eb5
Brian Mutinda <mutindabrian26@gmail.com>
2024-02-06 11:42:53 +0300

    create base view

---
+++ lib/ui/views/base_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:providers_architecture/core/viewmodels/base_model.dart';
+
+import '../../locator.dart';
+
+class BaseView<T extends BaseModel> extends StatefulWidget {
+  final Widget Function(BuildContext context, T model, Widget child) builder;
+  final Function(T)? onModelReady;
+
+  BaseView({required this.builder, required this.onModelReady});
+
+  @override
+  State<BaseView<T>> createState() => _BaseViewState<T>();
+}
+
+class _BaseViewState<T extends BaseModel> extends State<BaseView<T>> {
+  late T model;
+
+  @override
+  void initState() {
+    model = locator<T>();
+    if (widget.onModelReady != null) {
+      widget.onModelReady!(model);
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<T>(
+        create: (context) => locator<T>(),
+        child: Consumer<T>(
+            builder: (context, model, child) =>
+                widget.builder(context, model, child!),
+            child: const SizedBox()));
+  }
+}
--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:providers_architecture/core/viewmodels/login_model.dart';
-import 'package:providers_architecture/locator.dart';
+import 'package:providers_architecture/ui/shared/app_colors.dart';
 
 import '../../core/enums/view_state.dart';
 import '../widgets/login_header.dart';
+import 'base_view.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -18,36 +18,35 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<LoginModel>(
-      create: (context) => locator<LoginModel>(),
-      child: Consumer<LoginModel>(
-        builder: (context, model, child) => Scaffold(
-          body: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              LoginHeader(
-                controller: controller,
-                validationMessage: model.errorMessage,
-              ),
-              const SizedBox(height: 50),
-              model.state == ViewState.busy
-                  ? const CircularProgressIndicator()
-                  : ElevatedButton(
-                      child: const Text('Login'),
-                      onPressed: () async {
-                        var loginSuccess = await model.login(controller.text);
-                        if (loginSuccess) {
-                          if (context.mounted) {
-                            Navigator.pushNamed(context, '/');
-                          }
+    return BaseView<LoginModel>(
+      builder: (context, model, child) => Scaffold(
+        backgroundColor: backgroundColor,
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            LoginHeader(
+              controller: controller,
+              validationMessage: model.errorMessage,
+            ),
+            const SizedBox(height: 50),
+            model.state == ViewState.busy
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    child: const Text('Login'),
+                    onPressed: () async {
+                      var loginSuccess = await model.login(controller.text);
+                      if (loginSuccess) {
+                        if (context.mounted) {
+                          Navigator.pushNamed(context, '/');
                         }
-                      },
-                    ),
-            ],
-          ),
+                      }
+                    },
+                  ),
+          ],
         ),
       ),
+      onModelReady: null,
     );
   }
 }
